89b266af70a28cf26d991640f790c78805a2013e
pschombara <p.schombara@gmail.com>
2017-04-27 15:34:10 +0200

    add const for field

--- Field.cs
+++ Field.cs
@@ -8,5 +8,15 @@ namespace projectlf6
 {
     class Field
     {
+        const int FIELD_GRASS  = 0;
+        const int FIELD_DIRT = 1;
+        const int FIELD_STONE = 2;
+        const int FIELD_NO_BROCKEN = 3;
+        const int FIELD_COAL = 4;
+        const int FIELD_COPPER = 5;
+        const int FIELD_IRON = 6;
+        const int FIELD_SILVER = 7;
+        const int FIELD_GOLD = 8;
+        const int FIELD_DIAMOND = 9;
     }
 }
