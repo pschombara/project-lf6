3abc83db5fdb87bc066b1fde80cdd5041f7db9cc
pschombara <p.schombara@gmail.com>
2017-04-28 18:04:54 +0200

    add methods to player

--- Player.cs
+++ Player.cs
@@ -12,6 +12,12 @@ namespace projectlf6
         private Skin skin;
         private Score score;
         private Location location;
+        private int orientation;
+
+        const int ORIENTATION_UP = 0;
+        const int ORIENTATION_RIGHT = 1;
+        const int ORIENTATION_DOWN = 2;
+        const int ORIENTATION_LEFT = 3;
 
         public Player()
         {
@@ -19,6 +25,16 @@ namespace projectlf6
             this.skin = new Skin();
             this.score = new Score();
             this.location = new Location();
+            this.orientation = ORIENTATION_DOWN;
+        }
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.skin = new Skin();
+            this.score = new Score();
+            this.location = new Location();
+            this.orientation = ORIENTATION_DOWN;
         }
 
         public void setName(string name)
@@ -31,5 +47,49 @@ namespace projectlf6
             return this.name;
         }
 
+        public void setSkin(Skin skin)
+        {
+            this.skin = skin;
+        }
+
+        public Skin getSkin()
+        {
+            return this.skin;
+        }
+
+        public void setLocation(int xValue, int yValue)
+        {
+            //TODO set Location
+        }
+
+        public Location getLocation()
+        {
+            return this.location;
+        }
+
+        public void setOrientation(int orientation)
+        {
+            if (orientation >= ORIENTATION_UP && orientation <= ORIENTATION_LEFT) {
+                this.orientation = orientation;
+            } else {
+                this.orientation = ORIENTATION_DOWN;
+            }
+        }
+
+        public int getOrientation()
+        {
+            return this.orientation;
+        }
+
+        public bool saveToFile()
+        {
+            // TODO save to file
+            return true;
+        }
+
+        public void loadFromFile()
+        {
+            //TODO load from file
+        }
     }
 }
